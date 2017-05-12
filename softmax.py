e852c6aa637076edc081cb8a665539cf68dfb903
kaushik Visvanathan <kaushik.316@gmail.com>
2017-05-11 20:59:41 -0400

    Updated comment on indexing

--- softmax.py
+++ softmax.py
@@ -9,9 +9,6 @@ from dataproc import to_array, randomsample, to_onehot
 arr_train = to_array('data/train.csv')
 arr_test = to_array('data/test.csv')
 
-print (arr_train[0:, 0:10])
-
-print (arr_train[:,1:].shape)
 
 x = tf.placeholder(tf.float32, [None, 784])
 W = tf.Variable(tf.zeros([784, 10]))
@@ -31,22 +28,21 @@ with tf.Session() as sess:
 	# sess.add_tensor_filter("has_inf_or_nan", tf_debug.has_inf_or_nan)
 	tf.global_variables_initializer().run()
 
-	count = 0
 	for i in range(1000):
 		start_row, end_row = randomsample(arr_train, 100)
-		x_train = arr_train[start_row:end_row,1:]
+		# first column of arr_train is labels so we exlude that
+		x_train = arr_train[start_row:end_row, 1:] 
+		# want only the first column - i.e labels 0-9 so we can encode them as one-hots
 		y_raw = arr_train[start_row:end_row, 0]
 		y_train = to_onehot(y_raw)
 		sess.run(train_step, feed_dict={x: x_train, y_: y_train})
-		count = count + 1
-		print (count)
-
 
 
 	correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
 	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-	x_test = arr_test[:, 1:]
-	y_test = arr_test[:, 0]
+	x_test = arr_test[:, 0:]
+	y_raw = arr_test[:,0]
+	y_test = to_onehot(y_raw)
 
 	print(sess.run(accuracy, feed_dict={x: x_test, y_: y_test}))
