a0fd8a776db35cbeb15f6cd84cb593cae5584f90
kaushik Visvanathan <kaushik.316@gmail.com>
2017-05-11 20:26:35 -0400

    updated one hot function

--- dataproc.py
+++ dataproc.py
@@ -12,25 +12,21 @@ def to_array(filepath):
 def randomsample(array, interval):
 	max_index = len(array) - (1  + interval)
 	random_index = randint(0, max_index)
-
 	start_row = random_index
 	end_row = start_row + interval
 	return start_row, end_row
 
-##Takes a list of categories and returns a list of one hot encoded tuples where the first element is the 
-#Category and the second element is the one-hot encoding for that element
 
-def to_onehot(List):
-	final_list = []
-	for i in List:
-		position = List.index(i)
-		dummy_list = [0] * len(List)
-		dummy_list[position] = 1
-		final_list.append((i, dummy_list))
-	return final_list
+#Takes a list of numbers & returns one hot vectors for each based on value
+def to_onehot(array):
+	vectors = []
+	for num in array:
+		one_hot = np.zeros(10, dtype='float')
+		one_hot[num] = float(1) 
+		vectors.append(one_hot)
+	return vectors
+
 	
-#print to_onehot(["This", "that", "the other"])	
-
 
 
 
