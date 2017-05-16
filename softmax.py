91dadb42b690f6b7a4268a3effee652f811925ba
kaushik Visvanathan <kaushik.316@gmail.com>
2017-05-15 21:41:32 -0400

    added prediction generation

--- softmax.py
+++ softmax.py
@@ -4,11 +4,11 @@ import tensorflow as tf
 from random import randint
 from tensorflow.python import debug as tf_debug
 from dataproc import to_array, randomsample, to_onehot
+from tensorflow.examples.tutorials.mnist import input_data
 
 
 arr_train = to_array('data/train.csv')
 arr_test = to_array('data/test.csv')
-
 
 x = tf.placeholder(tf.float32, [None, 784])
 W = tf.Variable(tf.zeros([784, 10]))
@@ -37,12 +37,14 @@ with tf.Session() as sess:
 		y_train = to_onehot(y_raw)
 		sess.run(train_step, feed_dict={x: x_train, y_: y_train})
 
+	# Think the error is here because the training step appears to be running ok
+	# y_test results in a matrix of nans
+	x_test = arr_test[:, 0:]
+	feed_dict = {x: x_test}
+	y_test = sess.run(y, feed_dict)
+	print (y_test)
 
 	correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
 	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-	x_test = arr_test[:, 0:]
-	y_raw = arr_test[:,0]
-	y_test = to_onehot(y_raw)
-
-	print(sess.run(accuracy, feed_dict={x: x_test, y_: y_test}))
+	print ("Accuracy: %.2f " %( float(sess.run(accuracy, feed_dict={x: x_test, y_: y_test}))))
