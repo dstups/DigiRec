04c8824c2d0b5be2c241ace5147c56b22232b7ef
kaushik Visvanathan <kaushik.316@gmail.com>
2017-05-25 20:13:50 -0400

    Updated calculation of y to longer receive NaNs

--- softmax.py
+++ softmax.py
@@ -14,34 +14,29 @@ x = tf.placeholder(tf.float32, [None, 784])
 W = tf.Variable(tf.zeros([784, 10]))
 b = tf.Variable(tf.zeros([10]))
 
-y = tf.nn.softmax(tf.matmul(x, W) + b)
+y = tf.matmul(x,W) + b
 
 # tensor to hold cross-entropy results
 y_ = tf.placeholder(tf.float32, [None, 10])
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
 
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
  
 # create the session to run the graph
 with tf.Session() as sess:
-	# sess = tf_debug.LocalCLIDebugWrapperSession(sess)
-	# sess.add_tensor_filter("has_inf_or_nan", tf_debug.has_inf_or_nan)
 	tf.global_variables_initializer().run()
 
 	for i in range(1000):
 		start_row, end_row = randomsample(arr_train, 100)
 		# first column of arr_train is labels so we exlude that
 		x_train = arr_train[start_row:end_row, 1:] 
-		# want only the first column - i.e labels 0-9 so we can encode them as one-hots
 		y_raw = arr_train[start_row:end_row, 0]
 		y_train = to_onehot(y_raw)
 		sess.run(train_step, feed_dict={x: x_train, y_: y_train})
 
-	# Think the error is here because the training step appears to be running ok
-	# y_test results in a matrix of nans
-	x_test = arr_test[:, 0:]
+	x_test = arr_test
 	feed_dict = {x: x_test}
-	y_test = sess.run(y, feed_dict)
+	y_test = sess.run(y, feed_dict) # no more nan's but getting some crazy numbers here 
 	print (y_test)
 
 	correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
